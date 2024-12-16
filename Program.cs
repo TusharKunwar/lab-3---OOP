f8d16f4f83c3b51e2d6d4f7815461c56ee7cf4ca
TusharKunwar <tusharkunwar993@gmail.com>
2024-12-16 15:03:01 +0545

    new file added

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using lab_3;
+
+        // Creating objects
+        Human ban = new Human(10);
+        // ban.no_of_finger = 10;
+        ban.print_no_of_finger();
+        ban.FoodType = "NON-VEG";
+        ban.DisplayBehaviour();
---
+++ human.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_3
+{
+    public class Human
+    {
+        public int no_of_finger;
+        public string FoodType;
+
+        // Constructor
+        public Human(int noOfFinger)
+        {
+            no_of_finger = noOfFinger;
+        }
+
+        public void print_no_of_finger()
+        {
+            Console.WriteLine("No. of finger: " + no_of_finger);
+        }
+
+        public void DisplayBehaviour()
+        {
+            Console.WriteLine("He is " + FoodType);
+        }
+    }
+}
